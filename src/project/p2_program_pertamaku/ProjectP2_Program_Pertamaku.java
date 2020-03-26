94acc051434b3dc66b5f6a41d6cb81e94c904459
Ilham Eko Aris Saputro <62685938+ilhamcool@users.noreply.github.com>
2020-03-26 17:40:45 +0700

    Add files via upload

---
+++ src/project/p2_program_pertamaku/ProjectP2_Program_Pertamaku.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.p2_program_pertamaku;
+
+/**
+ *
+ * @author ilhamcool
+ */
+public class ProjectP2_Program_Pertamaku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame ut = new NewJFrame();
+      ut.setVisible(true);
+    }
+    
+}
